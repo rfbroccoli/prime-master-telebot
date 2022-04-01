8f5b85eda7126faf9dd135057395342ead082f4f
pwhb <pwhbdev@gmail.com>
2022-04-01 20:23:21 +0630

    moved bot into index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { app } = require("./server.js")
-const { bot, secretPath } = require("./bot.js")
+const { Telegraf } = require("telegraf");
+const { textHandler } = require("./controller.js");
 
-const { PORT, WEBHOOK_URL } = process.env
+const { PORT, BOT_TOKEN, WEBHOOK_URL } = process.env;
+
+if (!BOT_TOKEN) {
+  throw new Error("BOT_TOKEN must be provided!");
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+// Set the bot response
+bot.on("text", textHandler);
+
+const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 
 bot.telegram.setWebhook(`${WEBHOOK_URL}${secretPath}`);
 
